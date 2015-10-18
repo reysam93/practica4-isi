a3238b8bdecf09b6220ad258236477261224954d
reysam93 <samuel.rey.escudero@gmail.com>
2015-10-18 17:27:33 +0200

    testing if the new list name is a string

--- tests/jasmine/client/unit/PlayerSpec.js
+++ tests/jasmine/client/unit/PlayerSpec.js
@@ -2,15 +2,32 @@ describe('Meteor methods', function() {
 
   describe('createNewList', function() {
 
-    it("shouldn't add a new todo if not registered", function(done) {
-      var listName = "";
+    beforeEach(function(){
       spyOn(Lists, "insert");
+    });
 
+    it("shouldn't add a new list if not registered", function(done) {
+      var listName = "";
+      var err = "not logged-in";
+      
       Meteor.call("createNewList", listName, function(error, result){
+        expect(error.error).toBe(err);
         expect(Lists.insert).not.toHaveBeenCalled();
         done();
       });
     });
+
+    it("sholdn't add a new list if the name is not a string", function(done){
+      var listName = 47;
+      var err = "Match failed";
+
+      Meteor.call("createNewList", listName, function(error, result){
+        expect(error.reason).toBe(err);
+        expect(Lists.insert).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
 
   });
 
